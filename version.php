e3dc3e5bca9cf819e508cc00f83ebb62552eefcb
ennovhywinz <51473650+ennovhywinz@users.noreply.github.com>
2020-02-22 13:53:07 +0800

    Add files via upload

---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+ 
+$version=$obj->{'version'};
+ 
+ 
+$a=file_get_contents("updateVersion.txt");
+$intversion = (int)$a;
+ 
+echo $intversion ;
+  
+?>
